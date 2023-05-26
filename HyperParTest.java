012e74815feb8c108757456034bc5669fb8402aa
ivan <ivanind04@gmail.com>
2023-05-26 22:50:09 +0200

    todos los test pasados

--- HyperParTest.java
+++ HyperParTest.java
@@ -7,4 +7,24 @@ class HyperParTest {
         boolean prueba = HyperPar.esHyperPar(32342);
         assertEquals(false,prueba);
     }
+    @org.junit.jupiter.api.Test
+    void esHyperPar2() {
+        boolean prueba = HyperPar.esHyperPar(1);
+        assertEquals(false,prueba);
+    }
+    @org.junit.jupiter.api.Test
+    void esHyperPar3() {
+        boolean prueba = HyperPar.esHyperPar(-23);
+        assertEquals(false,prueba);
+    }
+    @org.junit.jupiter.api.Test
+    void esHyperPar4() {
+        boolean prueba = HyperPar.esHyperPar(-6);
+        assertEquals(true,prueba);
+    }
+    @org.junit.jupiter.api.Test
+    void esHyperPar5() {
+        boolean prueba = HyperPar.esHyperPar(6804);
+        assertEquals(true,prueba);
+    }
 }
